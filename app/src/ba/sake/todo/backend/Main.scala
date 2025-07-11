f331621b75a37d878ef21b9087c2a7611ca47507
Sakib Hadziavdic <sakib@sake.ba>
2025-07-11 17:13:05 +0200

    Update sharaf to 0.13.0

--- app/src/ba/sake/todo/backend/Main.scala
+++ app/src/ba/sake/todo/backend/Main.scala
@@ -1,42 +1,36 @@
 package ba.sake.todo.backend
 
 import java.util.UUID
-import io.undertow.Undertow
 import com.typesafe.config.ConfigFactory
+import ba.sake.sharaf.*
+import ba.sake.sharaf.undertow.UndertowSharafServer
 import ba.sake.tupson.*
 import ba.sake.tupson.config.*
 import ba.sake.validson.*
-import ba.sake.sharaf.handlers.cors.CorsSettings
-import ba.sake.sharaf.*, routing.*
 
-@main def main: Unit = {
+@main def main(): Unit = {
 
   val config = ConfigFactory.load.parseConfig[TodoBackendConfig]
   val todosRepo = new TodosRepo(config.baseUrl)
 
   val routes = Routes {
-    case GET() -> Path() =>
+    case GET -> Path() =>
       val res = todosRepo.getTodos().map(TodoResponse.fromTodo)
       Response.withBody(res)
-
-    case GET() -> Path("todos", param[UUID](id)) =>
+    case GET -> Path("todos", param[UUID](id)) =>
       val todo = todosRepo.getTodo(id)
       Response.withBody(TodoResponse.fromTodo(todo))
-
-    case POST() -> Path() =>
+    case POST -> Path() =>
       val reqBody = Request.current.bodyJsonValidated[CreateTodo]
       val newTodo = todosRepo.add(reqBody)
       Response.withBody(TodoResponse.fromTodo(newTodo))
-
-    case DELETE() -> Path() =>
+    case DELETE -> Path() =>
       todosRepo.deleteAll()
       Response.withBody(List.empty[TodoResponse])
-
-    case DELETE() -> Path("todos", param[UUID](id)) =>
+    case DELETE -> Path("todos", param[UUID](id)) =>
       todosRepo.delete(id)
       Response.withBody(todosRepo.getTodos().map(TodoResponse.fromTodo))
-
-    case PATCH() -> Path("todos", param[UUID](id)) =>
+    case PATCH -> Path("todos", param[UUID](id)) =>
       val reqBody = Request.current.bodyJsonValidated[PatchTodo]
       var todo = todosRepo.getTodo(id)
       reqBody.title.foreach(t => todo = todo.copy(title = t))
@@ -45,23 +39,15 @@ import ba.sake.sharaf.*, routing.*
       reqBody.order.foreach(o => todo = todo.copy(order = Some(o)))
       todosRepo.set(todo)
       Response.withBody(TodoResponse.fromTodo(todo))
-
   }
 
   val port = 8181
-
-  val server = Undertow
-    .builder()
-    .addHttpListener(port, "0.0.0.0")
-    .setHandler(
-      SharafHandler(routes).withCorsSettings(
-        CorsSettings.default.withAllowedOrigins(Set("https://todobackend.com"))
-      )
-    )
-    .build()
-
-  server.start()
-
+  UndertowSharafServer(
+    "0.0.0.0",
+    port,
+    routes,
+    corsSettings = CorsSettings.default.withAllowedOrigins(Set("https://todobackend.com"))
+  ).start()
   println(s"Started HTTP server at http://localhost:${port}")
 }
 
